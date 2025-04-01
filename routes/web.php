5d19467e7f650c08afbd3e216e0d8f536030376f
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2025-04-01 16:33:00 -0300

    feat: routes has been added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\CursosController;
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/cursos', [CursosController::class, 'index']);
+Route::get('/departamentos', [DepartamentoController::class, 'index']);
+Route::get('/contatos', [ContatoController::class, 'index']);
